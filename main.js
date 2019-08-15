ecb5c44342c4151ea3f5a792bdc5cc6f5d63f29d
Alexander <aheige17@gmail.com>
2019-08-15 14:56:22 +0200

    Changed to css grid to make html and css more clean and effective. Also added a keyPress feature to listen when user press key 0 - 9, for now it just outputs console logs. Its supposed to be connected to numpad.

--- main.js
+++ main.js
@@ -1,43 +1,70 @@
 window.onload = function () {
     
-    document.getElementById("equal").addEventListener("click", finish);
-    document.getElementById("col-ac").addEventListener("click", clearInput);
-    document.getElementById("col-ac").addEventListener("click", clearYarr);
+    document.getElementById("item-equ").addEventListener("click", finish);
+    document.getElementById("item-ac").addEventListener("click", clearInput);
+    document.getElementById("item-ac").addEventListener("click", clearYarr);
+    document.addEventListener("keydown", keyDown, false);
+
+function keyDown(e) {
+    let i = e.keyCode;
+    switch (i){
+        case 48:
+            console.log("0");
+            return;
+        case 49:
+            console.log("1")
+            return;
+        case 50:
+            console.log("2");
+            return;
+        case 51:
+            console.log("3");
+            return;
+        case 52:
+            console.log("4");
+            return;
+        case 53:
+            console.log("5");
+            return;
+        case 54:
+            console.log("6");
+            return;
+        case 55:
+            console.log("7");
+            return;
+        case 56:
+            console.log("8");
+            return;
+        case 57:
+            console.log("9");
+            return;
+        case 190:
+            console.log(".");
+            return;
+    }
+}
 
     let x = []; // bug-maker...
-    let operName = ['add', 'sub', 'mul', 'div']
-    let numPad = ['col-0','col-1', 'col-2', 'col-3', 
-                    'col-4', 'col-5','col-6','col-7',
-                    'col-8','col-9'];
+    let operName = ['item-add', 'item-sub', 'item-mul', 'item-div']
+    let numPad = ['item-0','item-1', 'item-2', 'item-3', 
+                    'item-4', 'item-5','item-6','item-7',
+                    'item-8','item-9'];
+                                
     let y = [];
     let answer = document.getElementById("answer");
-    let cc;
 
     for (let i = 0; i < numPad.length; i++){
         let a = document.getElementById(numPad[i])
         a.addEventListener("click", keyPress)
     }
 
-    function returnColorTime(){
-    setTimeout(send,50);
-    }
-
-    function send(){
-        returnColor(cc);
-    }
-
-    function returnColor(a){
-        $( a ).css("background-color", "#f5f5f0");
-    }
 
     function keyPress(){
         let p = document.getElementById("nr1");
         let c = this;
         let nr = this.innerHTML;
         let k = y.length - 1;
-        this.style.backgroundColor = '#999998';
-        returnColorTime();
-        cc = c;
+        //this.style.backgroundColor = '#999998';
             if(y.length < 1){
                 p.value = nr;
                 y.push(nr);
@@ -96,10 +123,10 @@ window.onload = function () {
         let d = document.getElementById("nr1")
         let c = document.getElementById('answer')
         let active = document.getElementsByClassName('active')[0]
-        console.log(calc(x[0], x[1], active.value));
+        console.log(calc(x[0], x[1], active.innerHTML));
         if (active){
-            c.innerHTML = x[0] + ' ' + active.value + ' ' + x[1] + " = " + calc(x[0], x[1], active.value)
-            d.value = calc(x[0], x[1], active.value)
+            c.innerHTML = x[0] + ' ' + active.innerHTML + ' ' + x[1] + " = " + calc(x[0], x[1], active.innerHTML)
+            d.value = calc(x[0], x[1], active.innerHTML)
         }
         focusInput()
         clearActive()
